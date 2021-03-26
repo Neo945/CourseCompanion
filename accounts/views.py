f7d53e9fe5fa4adfd429926487aac7ef611ddb62
Neo945 <shreeshsrvstv@gmail.com>
2021-03-26 21:48:28 +0530

    Subapplication fpr user added with the functions of login. logout and register auth page added to render the funcions

---
+++ accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+# Create your views here.
+
+def login_view(request):
+    form = AuthenticationForm(request,data=request.POST or None)
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request,user_)
+        return redirect('/')
+    return render(request,'accounts/auth.html',{"form":form,"button":"Login"})
+
+def reg_view(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        # user_ = form.get_user()
+        # login(request,user_)
+        print(form.cleaned_data)
+        u = form.save(commit=True)
+        # return redirect('/login')
+    return render(request,'accounts/auth.html',{"form":form,"button":"Register"})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        redirect('/login')
+    return render(request,'accounts/auth.html',{"form":None,"button":"Logout"})
--- coursecompanion/urls.py
+++ coursecompanion/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('course.urls',namespace='course'))
+    path('api/',include('course.urls',namespace='course')),
+    path('user/',include('accounts.urls',namespace='accounts'))
 ]
