ffc4ce9425274764cc35eff0f93ed05e7b5482c9
Neo945 <shreeshsrvstv@gmail.com>
2021-03-26 19:46:53 +0530

    Added course application Create model for course and video in course

---
+++ course/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+# Create your models here.
+class Course(models.Model):
+    name = models.CharField(null=False,max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # playlist = models.
+
+class Video(models.Model):
+    name = models.CharField(null=False,max_length=255)
+    # video = models.FileField(upload_to='/')
+    courses = models.ForeignKey(Course,on_delete=models.CASCADE)
